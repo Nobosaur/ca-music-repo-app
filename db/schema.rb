b0d500b5f3da00d1dae1da6ef3da106dd5ade290
Nobosaur <nkokotec@gmail.com>
2016-06-23 15:21:24 +0200

    Added User function

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160620203146) do
-
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+ActiveRecord::Schema.define(version: 20160623120152) do
 
   create_table "albums", force: :cascade do |t|
     t.string   "cover"
@@ -30,6 +27,15 @@ ActiveRecord::Schema.define(version: 20160620203146) do
     t.integer  "album_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
