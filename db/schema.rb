57b743ca40438dfbdf9792888685056a8030250a
Nobosaur <nkokotec@gmail.com>
2016-06-24 16:26:32 +0200

    Added user roles

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160623120152) do
+ActiveRecord::Schema.define(version: 20160624073926) do
 
   create_table "albums", force: :cascade do |t|
     t.string   "cover"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20160623120152) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "role"
   end
 
 end
